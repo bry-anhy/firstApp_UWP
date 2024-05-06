aac0316f95a2ada42b42e07f5b513647745ec210
BRYCEN-VN\a_y <a_y@brycen.com.vn>
2024-05-06 14:05:20 +0700

    update code bingding to collection of items

--- Recording.cs
+++ Recording.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 ///
 /// Example: Binding to a single item
+/// Example: Bingding to a collection of items
 ///
 namespace FirstAppUWP
 {
@@ -40,11 +42,37 @@ namespace FirstAppUWP
         /// Every binding consists of a binding target and a binding source. 
         /// Typically, the target is a property of a control or other UI element, 
         /// and the source is a property of a class instance (a data model, or a view model)
+        /// 
+        /// 
+        /// A common scenario is to bind to a collection of business objects. 
+        /// In C#, the generic ObservableCollection<T> class is a good collection choice for data binding, 
+        /// because it implements the INotifyPropertyChanged and INotifyCollectionChanged interfaces.
         /// </summary>
         public class RecordingViewModel
         {
             private Recording defaultRecording = new Recording();
             public Recording DefaultRecording { get { return defaultRecording;} }
+
+            ///
+            /// These interfaces provide change notification to bindings 
+            /// when items are added or removed or a property of the list itself changes. 
+            /// If you want your bound controls to update with changes to properties of objects in the collection, 
+            /// the business object should also implement INotifyPropertyChanged. 
+            /// For more info, see Data binding in depth.
+            ///
+            private ObservableCollection<Recording> recordings = new ObservableCollection<Recording>();
+            public ObservableCollection<Recording> Recordings { get { return recordings; } }
+
+            public RecordingViewModel() {
+                recordings.Add(
+                    new Recording() { ArtistName = "A1", CompositionName = "C1", ReleaseDateTime = new DateTime(2024, 05, 06) });
+
+                recordings.Add(
+                    new Recording() { ArtistName = "A2", CompositionName = "C2", ReleaseDateTime = new DateTime(2024, 05, 06) });
+
+                recordings.Add(
+                    new Recording() { ArtistName = "A3", CompositionName = "C3", ReleaseDateTime = new DateTime(2024, 05, 06) });
+            }
         }
     }
 }
