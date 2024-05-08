ae82cdd7c7dfd5bb1a21f96eadfbd86d67c969db
BRYCEN-VN\a_y <a_y@brycen.com.vn>
2024-05-08 14:52:34 +0700

    update code title bar customization  + Simple customization      -> Set title

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace FirstAppUWP
         public MainPage()
         {
             this.InitializeComponent();
+
+            // By default, the title bar shows the app's display name as the window title.
+            // The display name is set in the Package.appxmanifest file.
+            // To add custom text to the title, set the ApplicationView.Title property to a text value, as shown here.
+            ApplicationView.GetForCurrentView().Title = "Custom text";
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # firstApp_UWP
 this is application UWP
+
+1.User inteface
+https://learn.microsoft.com/en-us/windows/uwp/ui-input/title-bar
+  Title bar customization
+     Title bar components
+     This list describes the components of the standard title bar.
+
+         + Title bar rectangle
+         + Title text
+         + System menu - accessed by clicking the app icon or right-clicking the title bar
+         + Caption controls
+             -> Minimize button
+             -> Maximize/Restore button
+             -> Close button
+
+	* Simple
+    * Full
