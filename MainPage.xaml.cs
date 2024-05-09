904ef740d0eec1e8bb26600f7e64902ed2e56613
BRYCEN-VN\a_y <a_y@brycen.com.vn>
2024-05-09 09:43:15 +0700

    update code change color title bar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -31,6 +33,33 @@ namespace FirstAppUWP
             // The display name is set in the Package.appxmanifest file.
             // To add custom text to the title, set the ApplicationView.Title property to a text value, as shown here.
             ApplicationView.GetForCurrentView().Title = "Custom text";
+
+            ////////////////////////////////////////////////////////////
+            // The button background color is not applied to the close button hover and pressed states.
+            // The close button always uses the system-defined color for those states.
+            // Setting a color property to null resets it to the default system color.
+            // You can't set transparent colors. The color's alpha channel is ignored.
+
+            // using Windows.UI;
+            // using Windows.UI.ViewManagement;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            // Set active window colors
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.BackgroundColor = Colors.Green;
+            titleBar.ButtonForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = Colors.SeaGreen;
+            titleBar.ButtonHoverForegroundColor = Colors.White;
+            titleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
+            titleBar.ButtonPressedForegroundColor = Colors.Gray;
+            titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+
+            // Set inactive window colors
+            titleBar.InactiveForegroundColor = Colors.Gainsboro;
+            titleBar.InactiveBackgroundColor = Colors.SeaGreen;
+            titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+            titleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
+            ////////////////////////////////////////////////////////////
         }
     }
 }
