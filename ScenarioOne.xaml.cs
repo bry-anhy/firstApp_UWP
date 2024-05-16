7b6bc7a1df763fc366895945f6b66c7e3b0e97fd
BRYCEN-VN\a_y <a_y@brycen.com.vn>
2024-05-16 10:22:28 +0700

    update code   + add event button navigate to secondary page   + add event check/uncheck for enable back

--- ScenarioOne.xaml.cs
+++ ScenarioOne.xaml.cs
@@ -22,24 +22,50 @@ namespace FirstAppUWP
     /// </summary>
     public sealed partial class ScenarioOne : Page
     {
+        #region property
+        private static bool optedIn = false;
+        private MainPage rootPage;
+        #endregion property
+
         public ScenarioOne()
         {
             this.InitializeComponent();
+
+            // i want this page to be always cached
+            // so that we do not have to add logic to 
+            // save/restore state for the checkbox
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            rootPage = MainPage.Current;
         }
 
         private void CkEnableBack_Checked(object sender, RoutedEventArgs e)
         {
-
+            optedIn = !optedIn;
         }
 
         private void CkEnableBack_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            CkEnableBack_Checked(sender,e);
         }
 
+        /// <summary>
+        /// Navigate to Secondary page click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnNavigateToSecondaryPage_Click(object sender, RoutedEventArgs e)
         {
+            Frame rootFrame = Window.Current.Content as Frame;
 
+            // navigate to the next page, with info in the parameters
+            // wheter to enable the title bar UI or not
+            rootFrame.Navigate(typeof(SecondaryPage), optedIn);
         }
     }
 }
